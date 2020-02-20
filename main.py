0a62aac2206e78514aa59d3f5a65787b33752c46
Noba1anc3 <zxryhjp@yahoo.co.jp>
2020-02-20 17:46:14 +0800

    需求文档 added

--- main.py
+++ main.py
@@ -3,38 +3,58 @@ from preprocess.pdfminer import layout_analysis
 from preprocess.pdf2image import pdf2image
 from preprocess.half_full_judge import *
 
-from extraction.text.image_note import FigureNoteExtraction
-from extraction.text.table_note import TableNoteExtraction
-from extraction.text.author import AuthorExtraction
-from extraction.text.title import TitleExtraction
-from extraction.text.note import NoteExtraction
+from semseg.text.image_note import FigureNoteExtraction
+from semseg.text.table_note import TableNoteExtraction
+from semseg.text.author import AuthorExtraction
+from semseg.text.title import TitleExtraction
+from semseg.text.note import NoteExtraction
 
-from extraction.tools import *
+from semseg.tools import *
 from visualize.annotate import *
 
 from logzero import logger
 import numpy as np
+import argparse
 import sys
 import cv2
 import os
 
-status = 1
-#DEBUG状态下读取的是外目录下的pdf文件夹，并且每个文件处理完成后需要在控制台按任意键回车
-#按q键则会终止程序执行
-#RUN状态下读取的是内目录下'example/pdf_file/'下的文件，所有文件依次顺序处理
-#在每一页运行出结束后按任意键跳页即可，不需要中途干预
-
 if __name__ == '__main__':
-    if not os.path.exists('example/analysis_result/'):
-        os.mkdir('example/analysis_result/')
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--folder", type=str, default='../pdf/pdf_file/', help="pdf file's folder")
+    parser.add_argument("--filename", type=str, default='', help="specify a unique pdf file with '.pdf' ")
+    parser.add_argument("--debug", default=True, help="whether debug or not")
+    parser.add_argument("--seg_level", type=int, default=1,
+                        help="1: level-1 segmentation on text and image, 2: level-2 segmentation on text and image")
+    parser.add_argument("--table_level", type=int, default=1,
+                        help="1: only segment the bounding box of tables, 2: segment all the cells in the tables, additionally")
+    parser.add_argument("--tit_choice", type=int, default=0,
+                        help="0: segment all kinds of semantic type, 1: only segment text out, 2: only segment image out, 3: only segment table out")
+    parser.add_argument("--save_image", default=True, help="whether save the image result of segmentation")
+    parser.add_argument("--save_text", default=True, help="whether save the text result of segmentation")
+    opt = parser.parse_args()
+    print(opt)
 
-    if status == 1:
-        fileFolder = '../pdf/'
+    if opt.save_image == True or opt.save_text == True:
+        seg_result_folder = 'example/seg_result/'
+        if not os.path.exists(seg_result_folder):
+            os.mkdir(seg_result_folder)
+        if opt.save_image == True and not os.path.exists(seg_result_folder + 'image/'):
+            os.mkdir(seg_result_folder + 'image/')
+        if opt.save_text == True and not os.path.exists(seg_result_folder + 'text/'):
+            os.mkdir(seg_result_folder + 'text/')
+
+    if opt.debug == True:
+        fileFolder = opt.folder
     else:
         fileFolder = 'example/pdf_file/'
 
-    fileList = sorted(os.listdir(fileFolder))
-    fileNum = len(fileList)
+    if opt.filename == '':
+        fileList = sorted(os.listdir(fileFolder))
+        fileNum = len(fileList)
+    else:
+        fileList = [opt.filename]
+        fileNum = 1
 
     for index in range(fileNum):
         fileName = fileList[index]
@@ -88,7 +108,7 @@ if __name__ == '__main__':
             # Anno_Image = layoutImage(Anno_Image, PageLayout, liRatio)
             # cv2.imshow('img', Anno_Image)
 
-        if status == 1:
+        if opt.debug == True:
             c = str(input())
             if c == 'q':
                 sys.exit()
