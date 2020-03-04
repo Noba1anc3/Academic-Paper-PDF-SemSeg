7ae56612824259982764c0a50b9c29000a564e47
J_M <978916342@qq.com>
2020-03-04 21:38:46 +0800

    update table_detection.py and table_utils.py

--- semseg/table/table_detection.py
+++ semseg/table/table_detection.py
@@ -2,14 +2,13 @@ from pdfminer.layout import *
 
 from extraction.table.table_utils import *
 
-minimum_row_number = 1        # 表格中应最少包含的行数
-minimum_column_number = 1     # 表格中应最少包含的列数
+minimum_cell_number = 4       # 一个表格中应该至少包含多少个单元格
 horizontal_line_thres = 1     # 若两个水平线条的纵坐标之差小于horizontal_line_thres，则认为它们处于统一像素行
 alignment_thres = 1           # 表格的水平边界线的两个端点横坐标差值小于alignment_thres
 # 表格中不允许出现的词，用于减少识别false positive
-special_words=('table', 'figure')
+special_words=('table', 'Table', 'TABLE')
 # 算法块的标志性开头，因为算法被识别成表格的几率很高，所以单独处理
-algorithm_signal = ('algorithm', 'Algorithm')
+algorithm_signal = ('Algorithm', 'Figure')
 
 
 def merge_sort(lines):
@@ -44,33 +43,14 @@ def merge_sort(lines):
 
     return sorted_lines
 
-def get_grouping_number(nums):
-    '''
-    将一堆数字进行分组，距离小于thres4的为一组
-    :param nums: 一个列表，列表中存放多个数字
-    :return: 返回分成了多少组
-    '''
-    groups = []
-    for num in nums:
-        find_group = False
-        for group in groups:
-            if abs(num - group) < horizontal_line_thres:
-                find_group = True
-                break
-        if not find_group:
-            groups.append(num)
-    return len(groups)
 
-
-def detetct_singal_column(sorted_curves, special_lines, text_lines, algorithm_lines):
-    '''
-    检测出单栏中的所有表格
-    :param sorted_curves: 按照ｙ降序排列的本栏中的所有横线
-    :param special_lines: 含有特殊单词的文本行
-    :param text_lines: 所有文本行
-    :return: 单栏中的所有表格，格式为[[左上x, 左上y, 右下x, 右下y], ...]
-    '''
-    # 将线条进行对齐分组，同时每一组内部不能含有special_words
+def divide_curves_into_groups(sorted_curves, special_lines):
+    """
+    将水平线条分成多组，每一组作为表格的初始候选者
+    :param sorted_curves: 按y坐标降序排列的水平线条
+    :param special_lines: 不能出现在表格中的特殊字符，不会将它们划入同一个组内
+    :return: 根据一定规则初步分出的多个组
+    """
     groups = []
     start = 0
     while start < len(sorted_curves):
@@ -78,73 +58,120 @@ def detetct_singal_column(sorted_curves, special_lines, text_lines, algorithm_li
             if abs(sorted_curves[start].x0 - sorted_curves[end].x0) < alignment_thres and \
                     abs(sorted_curves[start].x1 - sorted_curves[end].x1) < alignment_thres:
 
-                # # 如果这两条线组成的矩形区域和特殊行有交集，那么就continue，否则，将这个区域加入备选组
-                # contain_special_lines = False
-                # for note in special_lines:
-                #     rectangle_region = [sorted_curves[start].x0, sorted_curves[start].y1,
-                #                         sorted_curves[end].x1, sorted_curves[end].y0]
-                #     note_region = [note.x0, note.y1, note.x1, note.y0]
-                #     if relation_between_regions(rectangle_region, note_region, 'INTERSECT'):
-                #         contain_special_lines = True
-                #         break
-                # if contain_special_lines:
-                #     continue
+                # 如果这两条线组成的矩形区域和特殊行有交集，那么就continue，否则，将这个区域加入备选组
+                contain_special_lines = False
+                rectangle_region = [sorted_curves[start].x0, sorted_curves[start].y1,
+                                    sorted_curves[end].x1, sorted_curves[end].y0]
+                for line in special_lines:
+                    note_region = [line.x0, line.y1, line.x1, line.y0]
+                    if relation_between_regions(rectangle_region, note_region, 'INTERSECT'):
+                        contain_special_lines = True
+                        break
 
-                group = sorted_curves[start:end + 1]
-                groups.append(group)
-                start = end + 1
-                break
+                if not contain_special_lines:
+                    group = sorted_curves[start:end + 1]
+                    groups.append(group)
+                    start = end + 1
+                    break
+    return groups
 
-    # 线条数量少于1的不可能是表格
-    groups_with_curves = []
+
+def exclude_algorithms(groups, algorithm_lines):
+    """
+    删除可能的算法框
+    :param groups: 原始的多个组
+    :param algorithm_lines: 含有算法标志的行
+    :return: 删除后剩余的多个组
+    """
+    remain_groups = []
     for group in groups:
+        group_region = [group[0].x0, group[0].y1, group[-1].x1, group[-1].y0]
+        contain = False
+        for element in algorithm_lines:
+            element_region = [element.x0, element.y1, element.x1, element.y0]
+            if relation_between_regions(group_region, element_region, 'CONTAIN'):
+                contain = True
+                break
+        if not contain:
+            remain_groups.append(group)
+    return remain_groups
+
+
+def exclude_figures_and_rects(groups, figures_and_rects):
+    """
+    删除组中可能的图片和非表格矩形框
+    :param groups: 原始的多个组
+    :param figures_and_rects: 图片和矩形框对象列表
+    :return: 删除后剩余的多个组
+    """
+    remain_groups = []
+    for group in groups:
+        group_region = [group[0].x0, group[0].y1, group[-1].x1, group[-1].y0]
+        contain = False
+        for element in figures_and_rects:
+            element_region = [element.x0, element.y1, element.x1, element.y0]
+            if relation_between_regions(element_region, group_region, 'CONTAIN'):
+                contain = True
+                break
+        if not contain:
+            remain_groups.append(group)
+    return remain_groups
+
+
+def get_cells_number(group, text_lines):
+    """
+    计算某个表格候选区域内的文字行的数量，用以大概估计单元格数量是否低于某个阈值
+    :param group: 表格候选者
+    :param text_lines: 当前页面所有文字行
+    :return: 区域内文字的数量
+    """
+    count = 0
+    group_region = [group[0].x0, group[0].y1, group[-1].x1, group[-1].y0]
+    for line in text_lines:
+        line_region = [line.x0, line.y1, line.x1, line.y0]
+        if relation_between_regions(group_region, line_region, 'CONTAIN'):
+            count += 1
+    return count
+
+
+def detetct_singal_column(sorted_curves, elements_dict):
+    """
+    检测出单栏中的所有表格
+    :param sorted_curves: 按照ｙ降序排列的本栏中的所有横线
+    :param elements_dict: 页面中其他元素成分组成的词典
+    """
+
+    # 将词典中的各成分提取出来
+    text_lines = elements_dict['text_lines']
+    figures = elements_dict['figures']
+    rects = elements_dict['rects']
+    special_lines = []
+    algorithm_lines = []
+    for line in text_lines:
+        if line.get_text().lower().startswith(special_words):
+            special_lines.append(line)
+        if line.get_text().startswith(algorithm_signal):
+            algorithm_lines.append(line)
+
+    # 将排好序的线条初步分组，每一组为一个表格区域的候选者
+    initial_groups = divide_curves_into_groups(sorted_curves, special_lines)
+
+    # 水平线条数量小于等于1的不可能是表格（后续可能需要优化的地方）
+    groups_with_curves = []
+    for group in initial_groups:
         if len(group) > 1:
             groups_with_curves.append(group)
 
     # 去除算法块
-    groups_without_algorithm = []
-    for group in groups_with_curves:
-        group_region = [group[0].x0, group[0].y1,
-                        group[-1].x1, group[-1].y0]
-        contain_algorithm = False
-        for line in algorithm_lines:
-            line_region = [line.x0, line.y1, line.x1, line.y0]
-            if relation_between_regions(group_region, line_region, 'CONTAIN'):
-                contain_algorithm = True
-        if not contain_algorithm:
-            groups_without_algorithm.append(group)
+    groups_without_algorithm_lines = exclude_algorithms(groups_with_curves, algorithm_lines)
 
+    # 去除图片和非表的矩形框
+    groups_without_figures_and_rects = exclude_figures_and_rects(groups_without_algorithm_lines, figures + rects)
 
-    # 表格内必须有多行多列textlines
+    # 表格中单元格数量必须达到一定阈值
     selected_groups = []
-    for group in groups_without_algorithm:
-        # 找出这个表格内的所有textline
-        text_regions = []
-        for text in text_lines:
-            rectangle_region = [group[0].x0, group[0].y0, group[-1].x1, group[-1].y1]
-            text_region = [text.x0, text.y1, text.x1, text.y0]
-            if relation_between_regions(rectangle_region, text_region, 'CONTAIN'):
-                text_regions.append(text)
-        if len(text_regions) == 0:
-            continue
-
-        # textlines的行数和列数应该大于一定阈值
-        # x分为左、中、右的原因是考虑到对齐方式可能是左对齐、居中、右对齐
-        x_left = []
-        x_mid = []
-        x_right = []
-        y = []
-        for region in text_regions:
-            x_left.append(region.x0)
-            x_right.append(region.x1)
-            x_mid.append((region.x0 + region.x1) / 2)
-            y.append(region.y0)
-        row_number = get_grouping_number(y)
-        column_number_left = get_grouping_number(x_left)
-        column_number_right = get_grouping_number(x_right)
-        column_number_mid = get_grouping_number(x_mid)
-        if row_number > minimum_row_number and column_number_left > minimum_column_number and \
-                column_number_right > minimum_column_number and column_number_mid > minimum_column_number:
+    for group in groups_without_figures_and_rects:
+        if get_cells_number(group, text_lines) >= minimum_cell_number:
             selected_groups.append(group)
 
     # 生成左上+右下坐标形式
@@ -155,27 +182,24 @@ def detetct_singal_column(sorted_curves, special_lines, text_lines, algorithm_li
 
 
 def detect_table(page_layout):
-    '''
+    """
     检测给定页面的所有表格
     :param page_layout: 由于pdfminer解析得到的布局信息
     :param special_words: 一些不可能出现在表格中的特殊词汇，用于减少false positive的数量
     :return: 当前页面的所有表格区域，格式为[[左上x, 左上y, 右下x, 右下y], ...]
-    '''
+    """
     # 从布局信息中提取可能需要用的box，水平横线、文字行、以为特殊字段开头的行
-    horizontal_curves = []
-    text_lines = []
-    special_lines = []
-    algorithm_lines = []
-    for box in page_layout:
-        if isinstance(box, LTCurve) and abs(box.y1 - box.y0) < horizontal_line_thres:
-            horizontal_curves.append(box)
-        if isinstance(box, LTTextBoxHorizontal):
-            for line in box:
-                text_lines.append(line)
-                if line.get_text().lower().startswith(special_words):
-                    special_lines.append(line)
-                if line.get_text().startswith(algorithm_signal):
-                    algorithm_lines.append(line)
+    horizontal_curves = get_horizontal_curves(page_layout)
+    text_lines = get_text_lines(page_layout)
+    figures = get_figures(page_layout)
+    rects = get_rects(page_layout)
+
+    # 将所有需要使用到的不同类别（除线条外）的页面元素放入字典中，使得参数列表更加清晰
+    elements_dict = {
+        'text_lines': text_lines,
+        'figures': figures,
+        'rects': rects
+    }
 
     # 将所有线条按照y坐标降序排列，便于后续操作
     sorted_curves = merge_sort(horizontal_curves)
@@ -188,8 +212,8 @@ def detect_table(page_layout):
             left_curves.append(line)
         else:
             right_curves.append(line)
-    left_tables = detetct_singal_column(left_curves, special_lines, text_lines, algorithm_lines)
-    right_tables = detetct_singal_column(right_curves, special_lines, text_lines, algorithm_lines)
-    tables = left_tables + right_tables
 
-    return tables
+    left_tables = detetct_singal_column(left_curves, elements_dict)
+    right_tables = detetct_singal_column(right_curves, elements_dict)
+
+    return left_tables + right_tables
